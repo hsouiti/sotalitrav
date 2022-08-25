a2913ebb8b907636bada76d77673f5bb5bee4055
Hassan <h.souiti@gmail.com>
2022-08-25 07:59:46 +0100

    adding plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,39 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'Sotalitrav',
+    descriptio: 'Societe de construction',
+    aufthor: '@Hassan',
+    siteUrl: `https://www.sotalitrav.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sotalitrav`,
+        short_name: `Sotalitrav`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
 }
 
 export default config
