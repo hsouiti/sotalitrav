c85ad3fe6894ed32833ef61c29e4297657d99cf0
Hassan <h.souiti@gmail.com>
2022-08-31 18:45:04 +0100

    structure the project

---
+++ content/nav-data.tsx
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    id: 1,
+    href: '/',
+    text: 'Accueil',
+  },
+  {
+    id: 2,
+    href: '/apropos',
+    text: 'A props de nous',
+  },
+  {
+    id: 3,
+    href: '/services',
+    text: 'Nos Services',
+  },
+  {
+    id: 4,
+    href: '/contact',
+    text: 'Contact',
+  },
+]
