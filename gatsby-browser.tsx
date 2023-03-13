b285d24e87fccbe0314e6c03cc0e1b771c9169e8
Hassan <h.souiti@gmail.com>
2023-03-13 11:55:14 +0100

    convert to nextjs

--- gatsby-browser.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-import './src/styles/global.css'
-
-import Layout from './src/layouts/layout'
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
