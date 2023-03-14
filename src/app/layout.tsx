3088f597ef4b653303a0604ac202d0a5cc847991
Hassan <h.souiti@gmail.com>
2023-03-14 09:20:38 +0100

    coming soon page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Inter, Ubuntu } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const ubuntu = Ubuntu({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={ubuntu.className}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+const color = '#9600ab'
 
 export default function Page() {
-  return <h1>Hello, From Next.js!</h1>;
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: '150px',
+      flexDirection: 'column'
+    }}>
+
+      <h1 style={{ color: color, marginBottom: '50px', fontSize: '1.7rem' }}>Site bientôt en ligne</h1>
+      <div style={{ textAlign: 'center' }}>
+        <h2 style={{ margin: 0, }}>Sotalitrav</h2>
+        <h4 style={{ color, fontWeight: '500', textTransform: 'uppercase', marginTop: '5px' }}>Amenagement - construction - negoce</h4>
+        <h4 style={{ fontWeight: '500', marginTop: '5px' }}>Email : contact.sotalitrav@gmail.com</h4>
+      </div>
+    </div>
+  );
 }
